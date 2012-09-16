b3c5c21a98ee4e77b255f8ba7190523a265a4393
Christoph Schindler <hop@30hopsmax.at>
2012-09-16 02:24:13 +0200

    Convert most of the scene graph to Intermission class.

--- src/scenes/intermissions.py
+++ src/scenes/intermissions.py
@@ -6,7 +6,39 @@ class Start(Intermission):
         self.next_scene = ("Intro", None)
 
         self.title = "ONE WHALE TRIP"
-        self.story = ["a love story", "in umpteen parts"]
+        self.story = ["a love story"]
 
         self.background = self.app.resman.get_background("beach")[0]
         self.creatures = [self.app.resman.get_sprite("whale_2-1")]
+
+
+class Intro(Intermission):
+    def _setup(self):
+        super(Intro, self)._setup()
+
+        self.next_scene = ("Game", None)
+        self.title = "INTRO"
+
+
+class GameOver(Intermission):
+    def _setup(self):
+        super(GameOver, self)._setup()
+
+        self.next_scene = ("Start", None)
+        self.title = "GAME OVER"
+
+
+class Victory(Intermission):
+    def _setup(self):
+        super(Victory, self)._setup()
+
+        self.next_scene = ("Outro", None)
+        self.title = "VICTORY"
+
+
+class Outro(Intermission):
+    def _setup(self):
+        super(Outro, self)._setup()
+
+        self.next_scene = ("GoodBye", None)
+        self.title = "OUTRO"
