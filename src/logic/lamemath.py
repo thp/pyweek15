ee4bdb3bd77aeb9019bcfc84626cf36812bc6b2a
Thomas Perl <m@thp.io>
2013-04-25 16:38:34 +0200

    Remove dead code in src/logic/lamemath.py

--- src/logic/lamemath.py
+++ src/logic/lamemath.py
@@ -7,15 +7,3 @@ def center(points):
     n = len(points)
     return float(sum_x) / n, float(sum_y) / n
 
-def center_in(surf, center_point):
-    """Center a point in a PyGame surface"""
-    x, y = center_point
-    w, h = surf.get_size()
-    return x-w/2, y-h/2
-
-def shade_color(color, pos, depth):
-    factor = math.pow(1.-float(pos)/float(depth), 1)
-    def shade(component):
-        return int(max(0, min(255, component*factor)))
-    return tuple(map(shade, color))
-
