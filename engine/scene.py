61b2adfb283ea72d29f5f8bf453058531a28d382
Thomas Perl <m@thp.io>
2016-07-09 21:04:11 +0200

    Creatures is empty list by default

--- engine/scene.py
+++ engine/scene.py
@@ -10,7 +10,7 @@ class Intermission(Scene):
         self.resume()
 
     def resume(self):
-        self.creatures = None
+        self.creatures = []
         self.skipable = False
         self._setup()
         self.update()
