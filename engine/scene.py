8fc4e7ac739ffbe9710f14ea9d595a3f6aec5dff
Thomas Perl <m@thp.io>
2016-07-09 22:23:42 +0200

    Particle system for victory screen

--- engine/scene.py
+++ engine/scene.py
@@ -1,7 +1,25 @@
+import random
+
 class Scene(object):
     def __init__(self, app, name):
         self.app = app
         self.name = name
+
+class Particle(object):
+    def __init__(self, sprite, x, y, sx, sy):
+        self.spawn = (x + random.randint(-sx, sx), y + random.randint(-sy, sy))
+        self.x, self.y = self.spawn
+        self.dy = -random.uniform(0.5, 3.0)
+        self.sprite = sprite
+        self.opacity = 1.0
+
+    def step(self):
+        self.y += self.dy
+        self.x += random.randint(-2, +2) / 10.0
+        self.opacity *= 0.97
+        if self.y < -self.sprite.h:
+            self.x, self.y = self.spawn
+            self.opacity = 1.0
 
 class Intermission(Scene):
     def __init__(self, app, name, filedata):
@@ -11,22 +29,31 @@ class Intermission(Scene):
 
     def resume(self):
         self.creatures = []
+        self.particles = []
         self.skipable = False
         self._setup()
         self.update()
 
     def process(self):
-        pass
+        for particle in self.particles:
+            particle.step()
 
     def update(self):
-        line = self.story.pop(0)
-        if line.startswith('(') and line.endswith(')'):
-            key, value = line[1:-1].split('=', 1)
-            if key == 'creatures':
-                self.creatures = [self.app.resman.creatures[c] for c in value.split()]
-            self.line = self.story.pop(0)
-        else:
-            self.line = line
+        while self.story:
+            line = self.story.pop(0)
+            if line.startswith('(') and line.endswith(')'):
+                key, value = line[1:-1].split('=', 1)
+                if key == 'creatures':
+                    self.creatures = [self.app.resman.creatures[c] for c in value.split()]
+                elif key == 'particles':
+                    values = value.split(':')
+                    sprite = values.pop(0)
+                    count = int(values.pop(-1))
+                    self.particles.extend(Particle(self.app.resman.creatures[sprite],
+                                                   *map(int, values)) for i in range(count))
+            else:
+                self.line = line
+                break
 
     def process_input(self, pressed, key):
         if pressed and ((self.skipable and key == 's') or not self.story):
@@ -36,6 +63,9 @@ class Intermission(Scene):
 
     def draw(self):
         self.app.screen.draw_card(self.title, self.line, self.background, self.creatures, self.skipable)
+        self.app.renderer.postprocess()
+        for particle in self.particles:
+            self.app.renderer.draw(particle.sprite, (particle.x, particle.y), particle.opacity, particle.opacity)
 
     def _setup(self):
         is_header = True
