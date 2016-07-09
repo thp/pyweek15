dcc1d461ff4b99d2c9cf3ebcb88d22fbdcd1f46c
Thomas Perl <m@thp.io>
2016-07-09 10:33:30 +0200

    Cleanup resman comments

--- src/engine/resman.py
+++ src/engine/resman.py
@@ -20,14 +20,12 @@ class ResourceManager():
         def _path(*relative):
             return os.path.join(current_dir, '..', '..', 'data', *relative)
 
-        ## load sprites
         for fn in sorted(glob.glob(_path('sprites', "*.png"))):
             bn, _ = os.path.splitext(os.path.basename(fn))
             surf = pygame.image.load(fn).convert_alpha()
             surf = self.app.renderer.register_sprite(bn, surf)
             self._surfaces[bn] = surf
 
-        ## load backgrounds
         for fn in sorted(glob.glob(_path('backgrounds', "*.jpg"))):
             bn, _ = os.path.splitext(os.path.basename(fn))
             key, frame = bn.split('-')
@@ -37,35 +35,29 @@ class ResourceManager():
             else:
                 self._backgrounds[key] = [surf]
 
-        ## load intermission assets
         for fn in glob.glob(_path('creatures', "*.png")):
             bn, _ = os.path.splitext(os.path.basename(fn))
             surf = pygame.image.load(fn)
             surf = surf.convert_alpha()
             self._creatures[bn] = surf
 
-        ## load sfx
         for fn in glob.glob(_path('sounds', '*.wav')):
             bn, _ = os.path.splitext(os.path.basename(fn))
             self.app.audman.register_sound(bn, fn)
 
-        ## load fonts
-        name, size = FONT_STD
-        font = pygame.font.Font(_path('fonts', '%s.ttf' % name), size)
-        self._fonts[FONT_STD] = font
+        for name, size in (FONT_STD, FONT_SMALL):
+            self._fonts[(name, size)] = pygame.font.Font(_path('fonts', '%s.ttf' % name), size)
 
         name, size = FONT_SMALL
         font = pygame.font.Font(_path('fonts', '%s.ttf' % name), size)
         self._fonts[FONT_SMALL] = font
 
-        ## load levels
         self.levels = []
         for fn in sorted(glob.glob(_path("levels", "*.txt"))):
             bn, _ = os.path.splitext(os.path.basename(fn))
             _, group, number = bn.split('-')
             self.levels.append((int(group), int(number), open(fn).read().splitlines()))
 
-        ## load intermissions
         self.intermissions = {}
         for filename in glob.glob(_path('intermissions', '*.txt')):
             name, _ = os.path.splitext(os.path.basename(filename))
